7f0364235638d535375d0a5969ccea8ad172be9c
Akpobome Elvis Esike <110521926+Casp3rr@users.noreply.github.com>
2023-09-13 12:13:48 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,115 +1,111 @@
-'use client';
-import { Hero } from "@/components";
-import SearchBar from '@/components/SearchBar';
-import CustomFilter from '@/components/CustomFilter';
-import { fetchCars } from '@/utils';
-import CarCard from '@/components/CarCard';
-import {fuels, yearsOfProduction} from "@/constants";
-import ShowMore from "@/components/ShowMore";
-import {useEffect, useState} from "react";
+"use client";
+
+import { useState, useEffect } from "react";
 import Image from "next/image";
+
+import { fetchCars } from "@/utils";
+import { fuels, yearsOfProduction } from "@/constants";
+import CarCard from "@/components/CarCard";
+import ShowMore from "@/components/ShowMore";
+import SearchBar from "@/components/SearchBar";
+import CustomFilter from "@/components/CustomFilter";
+import Hero  from "@/components/Hero";
+
+import { CarState } from "@/types";
 import { arrowdown } from "@/public ";
 
-export default  function Home(){
+export default function Home() {
+  const [allCars, setAllCars] = useState<CarState>([]);
+  const [loading, setLoading] = useState(false);
 
-    const [allCars, setAllCars ] = useState([]);
-    const [loading, setLoading] = useState(false);
+  // search states
+  const [manufacturer, setManuFacturer] = useState("");
+  const [model, setModel] = useState("");
 
-    // Search state
-  const [manufacturer, setManufacturer] = useState('');
-    const [model, setModel] = useState('');
+  // filter state
+  const [fuel, setFuel] = useState("");
+  const [year, setYear] = useState(2022);
 
-    // Filter state
-    const [fuel, setFuel] = useState('');
-    const [year, setYear] = useState('');
-
-    // Pagination state
+  // limit state
   const [limit, setLimit] = useState(10);
 
   const getCars = async () => {
-    setLoading(true)
-try{
-    const result = await fetchCars({
-      manufacturer:manufacturer || '',
-      year: year || 2022,
-      fuel: fuel || '',
-      limit: limit || 10,
-      model:model || '',
-  })
-  setAllCars(result)
-  }
-    catch(error){
-      console.log(error)
-    }finally {
-      setLoading(false)
-}
+    setLoading(true);
+    try {
+      const result = await fetchCars({
+        manufacturer: manufacturer.toLowerCase() || "",
+        model: model.toLowerCase() || "",
+        fuel: fuel.toLowerCase() || "",
+        year: year || 2022,
+        limit: limit || 10,
+      });
+
+      setAllCars(result);
+    } catch {
+      console.error();
+    } finally {
+      setLoading(false);
     }
+  };
 
   useEffect(() => {
-    console.log(fuel, year, manufacturer, model, limit)
-    getCars()
-  }, [manufacturer, model, fuel, year, limit])
-
-const isDataEmpty = !Array.isArray(allCars) || !allCars.length < 1 || !allCars;
+    getCars();
+  }, [fuel, year, limit, manufacturer, model]);
 
   return (
-    <main className="overflow-hidden">
+    <main className='overflow-hidden'>
       <Hero />
-      <div className='mt-12 padding-x padding-y max-width ' id='discover'>
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
         <div className='home__text-container'>
-          <h1 className='text-4xl font-extrabold'>
-            Car Catalogue
-          </h1>
-          <p >
-            Explore some of the best cars from around the world in our catalog.
-          </p>
-
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
         </div>
+
         <div className='home__filters'>
-          <SearchBar setManufacturer={setManufacturer} setModel={setModel} />
+          <SearchBar setManuFacturer={setManuFacturer} setModel={setModel} />
+
           <div className='home__filter-container'>
-            <CustomFilter title="fuel" options={fuels} setFilter={setFuel}/>
-            <CustomFilter title="year" options={yearsOfProduction} setFilter={setYear}/>
-
+            <CustomFilter options={fuels} setFilter={setFuel} />
+            <CustomFilter options={yearsOfProduction} setFilter={setYear} />
           </div>
-
         </div>
 
-        {allCars.length > 0? (
-        <section>
-          <div className='home__cars-wrapper'>
-            {allCars?.map((car) => (
-            <CarCard car={car}/>
-          ))}
-          </div>
-          {loading &&(
+        {allCars.length > 0 ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car, index) => (
+                <CarCard key={`car-${index}`} car={car} />
+              ))}
+            </div>
+
+            {loading && (
               <div className='mt-16 w-full flex-center'>
-
-              <Image
-                    src={arrowdown}
-                    alt="loader"
-                    width={50}
-                    height={50}
-                    className="object-contain"
-
-              />
+                <Image
+                  src={arrowdown}
+                  alt='loader'
+                  width={50}
+                  height={50}
+                  className='object-contain'
+                />
               </div>
-          )}
-          <ShowMore pageNumber={limit / 10}
-            isNext={ limit > allCars.length}
-            setLimit={setLimit}
-          />
-        </section>  
-        ): (
-          <div className='home__error-container'>
-           <h2 className='text-black text-xl font-bold'> NO Results Found </h2>
-           <p>{allCars?.message}</p>
-          </div>
-        )
-        }
+            )}
 
+            <ShowMore
+              pageNumber={limit / 10}
+              isNext={limit > allCars.length}
+              setLimit={setLimit}
+            />
+          </section>
+        ) : (
+          !loading && (
+            <div className='home__error-container'>
+              <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )
+        )}
       </div>
-
     </main>
-  )
+  );
 }
